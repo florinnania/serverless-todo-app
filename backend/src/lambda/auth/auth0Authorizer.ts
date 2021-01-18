042c91c4c1602c7a1f83a1049e16bc2a564e2bda
florin.nania <florin.nania@avira.com>
2021-01-18 23:16:19 +0200

    add Auth0 authorizer

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -9,10 +9,9 @@ import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = process.env.AUTH_0_JWKS_URL
+let cachedCertificate: string
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -58,10 +57,11 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const cert = await getCertificate(jwt.header.kid)
+
+  logger.info(`Verifying token ${token}`)
+
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +74,42 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+async function getCertificate(kid : string): Promise<string> {
+  if (cachedCertificate) return cachedCertificate
+
+  logger.info(`Fetching certificate from ${jwksUrl}`)
+
+  const response = await Axios.get(jwksUrl)
+  const keys = response.data.keys
+
+  if (!keys || !keys.length)
+    throw new Error('No JWKS keys found')
+
+  const signingKeys = keys.filter(
+    key => key.use === 'sig' // JWK property `use` determines the JWK is for signature verification
+        && key.kty === 'RSA' // We are only supporting RSA (RS256)
+        && key.kid           // The `kid` must be present to be useful for later
+        && ((key.x5c && key.x5c.length) || (key.n && key.e)) // Has useful public keys
+  )
+
+  if (!signingKeys.length)
+    throw new Error('No JWKS signing keys found')
+  
+  const signingKey = signingKeys.find(key => key.kid === kid);
+  const pub = signingKey.x5c[0]  // public key
+
+  // Certificate found!
+  cachedCertificate = certToPEM(pub)
+
+  logger.info('Valid certificate found', cachedCertificate)
+
+  return cachedCertificate
+}
+
+function certToPEM(cert: string): string {
+  cert = cert.match(/.{1,64}/g).join('\n')
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`
+  return cert
 }
